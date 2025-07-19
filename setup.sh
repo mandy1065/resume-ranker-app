42105cf9a068a5d28e8f3e4d536ec2055e4533a0
mandy1065 <msdayal85@gmail.com>
2025-07-19 09:29:49 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python -m nltk.downloader punkt stopwords averaged_perceptron_tagger wordnet maxent_ne_chunker words
+python -m nltk.downloader stopwords
 python -m spacy download en_core_web_sm
-
